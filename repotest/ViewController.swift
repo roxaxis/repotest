ec0609582aece6c3aded72af1805646c21f59044
Ahmet Hasırcıoğlu <ahasircioglu@Ahmets-MacBook-Pro.local>
2015-06-10 16:19:39 +0300

    Initial Commit

---
+++ repotest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  repotest
+//
+//  Created by Ahmet Hasırcıoğlu on 10/06/15.
+//  Copyright (c) 2015 Ahmet Hasırcıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
